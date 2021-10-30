63b59d63d7569b230a67a5399c508b8012d98bc5
Your Name <you@example.com>
2021-10-30 12:50:03 +0600

    update api

--- index.js
+++ index.js
@@ -24,6 +24,8 @@ async function run(){
         await client.connect();
         const database = client.db('foodWala');
         const foodCollections = database.collection('foods');
+        const orderCollection = database.collection('orders');
+        const myOrderCollection = database.collection('myOrders');
 
 // GET API
 app.get('/foods', async(req, res)=>{
@@ -50,7 +52,85 @@ app.get('/foods/:id', async(req, res)=>{
             console.log(result);
             res.json(result);
         });
+
+// GET ORDERS API
+app.get('/orders', async(req, res)=>{
+    const cursor = orderCollection.find({});
+    const orders = await cursor.toArray();
+    res.send(orders);
+})
+
+          // ADD ORDERS API
+    app.post('/orders', async(req, res)=>{
+        const order = req.body;
+        const result = await orderCollection.insertOne(order);
+        res.json(result);
+    })
+
+
+
+
+    // Cancel MY ORDER API
+app.delete('/foods/:id', async(req,res)=>{
+    const id= req.params.id;
+    const query= {_id:ObjectId(id)};
+    const result = await foodCollections.deleteOne(query);
+    res.json(result);
+})
+
+// GET MY ORDERS API
+app.get('/myOrders', async(req, res)=>{
+    const cursor = myOrderCollection.find({});
+    const orders = await cursor.toArray();
+    res.send(orders);
+})
+
+
+// ADD MY ORDERS
+app.post('/myOrders',(req, res)=>{
+    console.log(req.body);
+    myOrderCollection.insertOne(req.body).then((result)=>{
+       res.send(result);
+    })
+})
+
+
+// GET MY ORDERS with email
+app.get('/myOrders/:email', async(req,res)=>{
+    console.log(req.params.email);
+
+    const result = await myOrderCollection.find({email: req.params.email}).toArray();
+    res.send(result);
+
+})
+
+
+
+
+// DELETE MY ORDER API
+app.delete('/myOrders/:id', async(req, res)=>{
+    const id = req.params.id;
+    console.log(id);
+    const query = {_id: ObjectId(id)};
+    const result = await myOrderCollection.deleteOne(query);
+    res.json(result);
+})
+
+
+
+
+
+// DELETE ANYONE'S ORDER API
+app.delete('/orders/:id', async(req, res)=>{
+    const id = req.params.id;
+    const query = {_id: ObjectId(id)};
+    const result = await orderCollection.deleteOne(query);
+    res.json(result);
+})
+
     }
+
+
     finally{
         // await client.close();
     }
